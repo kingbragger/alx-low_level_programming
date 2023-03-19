160c94158e022422627d9a6fb3063db595785ca4
Kingbragger <kingbragger/zero_day.git>
2023-03-19 13:16:11 +0200

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet.
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     int i;
+
+     for (i = 0 < 52; i++)
+     {
+	     putchar(alp[i]);
+     }
+     putchar('\n');
+     return (0);
+}
