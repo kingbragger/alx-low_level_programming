5da8eb3e8e8d06744a62cfb54b5b71fb7101f2b8
Kingbragger <kingbragger/zero_day.git>
2023-03-24 23:05:28 +0200

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - print 0
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+        char c;
+        for (c = '0'; c <= '9' c++ ) ;
+        {
+                '_putcha'r(o);
+        }
+        'putcha'r('\n');
+}
