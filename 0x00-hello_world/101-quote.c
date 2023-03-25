468f627ab009a5a017cce1ec7de6fcc0f9235ef2
Kingbragger <kingbragger/zero_day.git>
2023-03-25 18:30:11 +0200

    Hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,26 @@
-#include <studio.h>
+#include <stdio.h>
+
 #include <unistd.h>
 
+
 /**
- *  main - entry point
- *
- * Return: Always 1 (success)
- */
+
+* main - Entry point
+
+*
+
+* Return: Always 1 (Success)
+
+*/
+
 int main(void)
+
 {
-	write(2,
-	"and that piece of art is useful\\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}	
+
+        write(2,
+
+        "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+        return (1);
+
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
+
 /**
- * main - entry point
- *
- * Return: Always 0 (success)
- */
+
+* main - Entry point
+
+*
+
+* Return: Always 0 (Success)
+
+*/
+
 int main(void)
+
 {
-	printf("with proper grammer, but the outcome is piece of art,\n");
-	return (0)
+
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+
+        return (0);
+
 }		
