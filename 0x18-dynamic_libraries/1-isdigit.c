570dd9f0143d5040332b27166ef4686798922d3a
Kingbragger <ndlovuhenry73@gmail.com>
2023-08-25 21:07:27 +0200

    updatted

--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _isdigit - checks if numbers are 0 - 9
+ * 1_isdigit - checks if numbers are 0 - 9
  * @c: char to be checked
  *
  * Return: 0 or 1
  */
 
-int _isdigit(int c)
+int 1_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
